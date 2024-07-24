bbabb3db9b657849050e8541392a1d2d6c726c84
ianbalijawa <ianbalijawa16@gmail.com>
2024-07-24 14:06:23 +0300

    [core:http] adjust request hook into more cleaner readable code

--- src/hooks/use-request.ts
+++ src/hooks/use-request.ts
@@ -1,63 +1,44 @@
-import axios, { AxiosInstance, AxiosResponse } from 'axios';
+import axios, { AxiosInstance, AxiosError } from 'axios';
 import { notifications } from '@mantine/notifications';
 import { useSelector } from 'react-redux';
 import { RootState } from '../app/store';
 import { __prod__ } from '../utils/__prod__';
 
-export default function useRequest(requireAuth: boolean): AxiosInstance {
+const BASE_URL = __prod__
+	? import.meta.env.VITE_APP_BASE_URL_PROD
+	: import.meta.env.VITE_APP_BASE_URL_DEV;
+
+export default function useRequest(requireAuth: boolean = false): AxiosInstance {
 	const token = useSelector((state: RootState) => state.auth.token);
 
-	const instance = !requireAuth
-		? axios.create({
-				baseURL: __prod__
-					? import.meta.env.VITE_APP_BASE_URL_PROD!
-					: import.meta.env.VITE_APP_BASE_URL_DEV!,
-			})
-		: axios.create({
-				baseURL: __prod__
-					? import.meta.env.VITE_APP_BASE_URL_PROD!
-					: import.meta.env.VITE_APP_BASE_URL_DEV!,
-				headers: {
-					Authorization: `Bearer ${token}`,
-				},
-			});
-
-	instance.interceptors.request.use(
-		(config) => {
-			return config;
-		},
-		(error) => {
-			return Promise.reject(error);
-		}
-	);
+	const instance = axios.create({
+		baseURL: BASE_URL,
+		headers: requireAuth ? { Authorization: `Bearer ${token}` } : {},
+	});
 
 	instance.interceptors.response.use(
-		(response: AxiosResponse) => {
-			return response;
-		},
-		(error) => {
+		(response) => response,
+		(error: AxiosError) => {
+			let title = 'Error';
+			let message = 'An unexpected error occurred';
+
 			if (error.response) {
-				notifications.show({
-					title: 'Error',
-					autoClose: 5000,
-					message: error.response?.data.message,
-					color: 'red',
-				});
+				title = `Error ${error.response.status}`;
+				// @ts-ignore
+				message = error.response.data?.message || 'An error occurred with the response';
 			} else if (error.request) {
-				notifications.show({
-					autoClose: 5000,
-					title: 'Error status :: ' + error.response.status,
-					message: 'Request was made, No response received',
-					color: 'red',
-				});
+				message = 'No response received from the server';
 			} else {
-				notifications.show({
-					autoClose: 5000,
-					title: 'Error status :: ' + error.response.status,
-					message: 'Error setting up the request:: ' + error.message,
-					color: 'red',
-				});
+				message = `Error setting up the request: ${error.message}`;
 			}
+
+			notifications.show({
+				title,
+				message,
+				color: 'red',
+				autoClose: 5000,
+			});
+
 			return Promise.reject(error);
 		}
 	);
