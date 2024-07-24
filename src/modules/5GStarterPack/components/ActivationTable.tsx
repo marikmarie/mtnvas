a3cab3101be984ea260434fa07a56545d1c67e9e
ianbalijawa <ianbalijawa16@gmail.com>
2024-07-24 14:07:11 +0300

    refactor to use react-query

--- src/modules/5GStarterPack/components/ActivationTable.tsx
+++ src/modules/5GStarterPack/components/ActivationTable.tsx
@@ -1,4 +1,4 @@
-import { memo, useCallback, useEffect, useState } from 'react';
+import { memo, useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { setServiceCode, setSubscriptionId } from '../../../app/slices/bundle-activations';
 import useRequest from '../../../hooks/use-request';
@@ -6,6 +6,8 @@ import { toTitle } from '../../../utils/to-title';
 import { useDataGridTable } from '../../../hooks/use-data-grid-table';
 import { ActionIcon } from '@mantine/core';
 import { IconCircleCheck } from '@tabler/icons-react';
+import { useQuery } from '@tanstack/react-query';
+
 type Data = {
 	subscriptionId: string;
 	msisdn: string;
@@ -14,48 +16,48 @@ type Data = {
 	createdAt: string;
 };
 
-export default memo(() => {
+const BundleActivationsReport = () => {
 	const dispatch = useDispatch();
 	const request = useRequest(true);
-	const [loading, setLoading] = useState(false);
-	const [activations, setActivations] = useState<{ data: Data[] }>({ data: [] });
 
-	const onSelect = useCallback((data: any) => {
-		dispatch(setSubscriptionId(data['subscriptionId']));
-		dispatch(setServiceCode(data['serviceCode']));
-	}, []);
+	const fetchBundleActivations = useCallback(async () => {
+		const response = await request.get('/bundle-activations');
+		return response.data as { data: Data[] };
+	}, [request]);
 
-	const columns = ['subscriptionId', 'msisdn', 'email', 'serviceCode', 'createdAt'].map(
-		(column) => {
-			if (column === 'createdAt') {
-				return {
-					name: column,
-					header: 'PERFORMED AT',
-					defaultFlex: 1,
-					// @ts-ignore
-					render: ({ data }) => (
-						<>
-							{new Date(data['createdAt']).toLocaleDateString('en-UK')}{' '}
-							{new Date(data['createdAt']).toLocaleTimeString('en-UK')}
-						</>
-					),
-				};
-			}
-			return {
-				name: column,
-				header: toTitle(column),
-				defaultFlex: 1,
-			};
-		}
+	const { data: activations, isLoading } = useQuery(
+		['bundleActivations'],
+		fetchBundleActivations
 	);
 
-	columns.push({
-		name: 'action_select',
-		header: 'ACTION/SELECT',
-		defaultFlex: 1,
-		// @ts-ignore
-		render({ data }) {
-			return (
+	const onSelect = useCallback(
+		(data: Data) => {
+			dispatch(setSubscriptionId(data.subscriptionId));
+			dispatch(setServiceCode(data.serviceCode));
+		},
+		[dispatch]
+	);
+
+	const columns = [
+		...['subscriptionId', 'msisdn', 'email', 'serviceCode', 'createdAt'].map((column) => ({
+			name: column,
+			header: column === 'createdAt' ? 'PERFORMED AT' : toTitle(column),
+			defaultFlex: 1,
+			render:
+				column === 'createdAt'
+					? ({ data }: { data: Data }) => (
+							<>
+								{new Date(data.createdAt).toLocaleDateString('en-UK')}{' '}
+								{new Date(data.createdAt).toLocaleTimeString('en-UK')}
+							</>
+						)
+					: undefined,
+		})),
+		{
+			name: 'action_select',
+			header: 'ACTION/SELECT',
+			defaultFlex: 1,
+			render: ({ data }: { data: Data }) => (
 				<ActionIcon
 					onClick={() => onSelect(data)}
 					variant="outline"
@@ -64,29 +66,18 @@ export default memo(() => {
 				>
 					<IconCircleCheck /> Select
 				</ActionIcon>
-			);
+			),
 		},
-	});
+	];
 
 	const activationsReportTable = useDataGridTable({
-		columns: columns,
-		data: activations.data,
-		loading,
+		columns,
+		data: activations?.data || [],
+		loading: isLoading,
 		mih: '60vh',
 	});
 
-	const getBundleActivations = useCallback(async () => {
-		try {
-			setLoading(true);
-			const response = await request.get('/bundle-activations');
-			setActivations(response.data as unknown as { data: Data[] });
-			setLoading(false);
-		} catch (error) {}
-	}, []);
-
-	useEffect(() => {
-		getBundleActivations();
-	}, []);
-
 	return activationsReportTable;
-});
+};
+
+export default memo(BundleActivationsReport);
