7f2c6aec690a1cef7d1397b6ea069ffd62c1de0e
Ian balijawa <you@example.com>
2025-07-31 16:38:53 +0300

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -31,7 +31,6 @@ import {
 	IconRouter,
 	IconUser,
 	IconUsers,
-	IconUsersGroup,
 } from '@tabler/icons-react';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -363,18 +362,18 @@ const navItems = [
 			},
 		],
 	},
-	{
-		label: 'Dealers',
-		icon: <IconUsersGroup size={20} />,
-		items: [
-			{
-				key: 'dealer-management',
-				label: 'Dealer Management',
-				path: '/dealer-management',
-				icon: <IconUsersGroup size={16} />,
-			},
-		],
-	},
+	// {
+	// 	label: 'Dealers',
+	// 	icon: <IconUsersGroup size={20} />,
+	// 	items: [
+	// 		{
+	// 			key: 'dealer-management',
+	// 			label: 'Dealer Management',
+	// 			path: '/dealer-management',
+	// 			icon: <IconUsersGroup size={16} />,
+	// 		},
+	// 	],
+	// },
 ];
 
 export function Header() {
